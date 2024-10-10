a1e1d91a092b08eb943f0fa782aa121da7d4b792
Sandy <syjy8809@gmail.com>
2024-10-10 16:34:43 +0900

    :sparkle:  몬스터 프리팹

--- Assets/MonsterSpawner.cs
+++ Assets/MonsterSpawner.cs
@@ -6,8 +6,9 @@ public class MonsterSpawner : MonoBehaviour
 {
     public Vector2 SkeletonSpawnPoint = new Vector2(5, -2.7f);
     public Vector2 EliteOrcSpawnPoint = new Vector2(5, -2.4f);
-    public Vector2 WizardSpawnPoint = new Vector2(5, -2.7f);
-    public Vector2 WerebearSpawnPoint = new Vector2(5, -2.7f);
+    public Vector2 WizardSpawnPoint = new Vector2(5, -2.6f);
+    public Vector2 WerebearSpawnPoint = new Vector2(5, -2.6f);
+    public Vector2 OrcriderSpawnPoint = new Vector2(5, -2.7f);
 
     public string Skeleton = "MON0001";
     public string EliteOrc = "MON0002";
@@ -15,10 +16,19 @@ public class MonsterSpawner : MonoBehaviour
     public string Werebear = "MON0004";
     public string Orcrider = "MON0005";
 
+    public List<GameObject> MonsterList = new List<GameObject>();
+
     private void Start()
     {
-        string id = DataManager.Instance.ReadOnlyDataSystem.MonsterDic[Skeleton].Id;
-        Debug.Log(id);
-    }
+        /*string id = DataManager.Instance.ReadOnlyDataSystem.MonsterDic[Skeleton].Id;
+        Debug.Log(id);*/
 
+        foreach (var monster in DataManager.Instance.ReadOnlyDataSystem.MonsterDic)
+        {
+            GameObject prefab = ResourceManager.Instance.Load<GameObject>($"Prefabs/Monster/{monster.Value.Id}");
+            GameObject monsterObj = Instantiate(prefab);
+            monsterObj.transform.position = SkeletonSpawnPoint;
+            MonsterList.Add(monsterObj);
+        }
+    }
 }
