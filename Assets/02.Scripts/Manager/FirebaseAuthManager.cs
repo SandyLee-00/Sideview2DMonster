eadd74fb71da292014dc864e16ffa09c97340eea
Sandy <syjy8809@gmail.com>
2024-10-11 15:27:57 +0900

    :sparkle: 파이어베이스 로그인 이메일

--- Assets/02.Scripts/Manager/FirebaseAuthManager.cs
+++ Assets/02.Scripts/Manager/FirebaseAuthManager.cs
@@ -1,18 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Firebase.Auth;
+using UnityEngine.UI;
+using TMPro;
+using Firebase.Extensions;
 
 public class FirebaseAuthManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private FirebaseAuth auth;
+    private FirebaseUser user;
+
+    public TMP_InputField EmailInputField;
+    public TMP_InputField PasswordInputField;
+
+    private void Awake()
     {
-        
+        auth = FirebaseAuth.DefaultInstance;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CreateAccount()
     {
-        
+        auth.CreateUserWithEmailAndPasswordAsync(EmailInputField.text, PasswordInputField.text).ContinueWith(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("FirebaseAuthManager::CreateAccount() 회원가입 취소");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("FirebaseAuthManager::CreateAccount() 회원가입 실패");
+                return;
+            }
+
+            user = task.Result.User;
+            Debug.Log("FirebaseAuthManager::CreateAccount() 회원가입 성공");
+        });
+    }
+
+    public void Login()
+    {
+        auth.SignInWithEmailAndPasswordAsync(EmailInputField.text, PasswordInputField.text).ContinueWith(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("FirebaseAuthManager::Login() 로그인 취소");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("FirebaseAuthManager::Login() 로그인 실패");
+                return;
+            }
+
+            user = task.Result.User;
+            Debug.Log("FirebaseAuthManager::Login() 로그인 성공");
+        });
+    }
+
+    public void Logout()
+    {
+        auth.SignOut();
+        Debug.Log("FirebaseAuthManager::Logout() 로그아웃");
     }
 }
